34d62546af4f4698368c31bfdd1e565fd9b4f326
neelkanth.vyas <neelkanth.vyas@sunarctechnologies.com>
2018-05-01 12:11:59 +0530

    dbs

--- app/src/main/java/com/example/sunarcneel/final_git_project/MainActivity.java
+++ app/src/main/java/com/example/sunarcneel/final_git_project/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //happy conflict kbbkjhvdf fdhvf
+        //bfjks jhhbfsd git
     }
 }
