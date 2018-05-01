7e166539102b4b97c122e72fb14e8141f045183c
neelkanth.vyas <neelkanth.vyas@sunarctechnologies.com>
2018-05-01 11:52:14 +0530

    neel two

--- app/src/main/java/com/example/sunarcneel/final_git_project/MainActivity.java
+++ app/src/main/java/com/example/sunarcneel/final_git_project/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello neel git upload
+        //hello neel git upload neel two
     }
 }
